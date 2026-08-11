0abf04da32c40756f9c584571f365b4383f38b42
Erin3012 <ericxxnn20@gmail.com>
2026-08-11 10:43:14 -0400

    Corregir contenedor de datos en Dompdf

--- tests/PayslipPdfTest.php
+++ tests/PayslipPdfTest.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 require_once __DIR__.'/../app/PayslipPdf.php';
 
-$html='<article class="payslip-reference"><h1>LIQUIDACIÓN DE SUELDO</h1><p>Prueba Dompdf</p></article>';
+$html='<article class="payslip-reference"><h1>LIQUIDACIÓN DE SUELDO</h1><section class="employee-data"><div><b>RUT:</b> 1-9</div><div><b>Nombre:</b> Prueba</div><div><b>Cargo:</b> Cargo</div><div><b>Fecha ingreso:</b> 01/01/2026</div></section><p>Prueba Dompdf</p></article>';
 $css=(string)file_get_contents(__DIR__.'/../public/style.css');
 $pdf=PayslipPdf::renderHtml($html,$css);
 if(!str_starts_with($pdf,"%PDF-"))throw new RuntimeException('PDF header inválido');
